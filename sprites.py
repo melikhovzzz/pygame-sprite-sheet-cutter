c0f8b64922e3796217989ae3784ae7d630e819cf
Gregory Melikhov <gr.melikhov12@gmail.com>
2025-11-17 17:11:43 +0200

    Update sprite sheet image path and comments

--- sprites.py
+++ sprites.py
@@ -9,7 +9,7 @@ SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Spritesheets')
 
-sprite_sheet_image = pygame.image.load('E:\PythonProjects\sprites\Sprite Sheets\Skeleton Idle.png').convert_alpha()
+sprite_sheet_image = pygame.image.load('PATH_TO_YOUR_IMAGE').convert_alpha()
 sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
 
 BG = (50, 50, 50)
@@ -21,13 +21,13 @@ frame_0 = sprite_sheet.get_image(0, 22, 32, 4, BLACK)
 run = True
 while run:
 
-    #update background
+    # update background
     screen.fill(BG)
 
-    #show frame image
+    # frame image display
     screen.blit(frame_0, (10, 10))
 
-    #event handler
+    # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
